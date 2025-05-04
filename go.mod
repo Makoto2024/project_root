b1f72057cebb0737dfd1bd61bd5ecdcef22b9db5
Patrick Liao <liaopatrick@google.com>
2025-05-04 10:21:37 +0000

    Add server main.

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/Makoto2024/project_root
 
 go 1.24.2
 
-require google.golang.org/protobuf v1.36.6
+require (
+	github.com/golang/glog v1.2.5
+	google.golang.org/grpc v1.72.0
+	google.golang.org/protobuf v1.36.6
+)
+
+require (
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+)
---
+++ golang_server/cmd/server/main.go
@@ -0,0 +1,60 @@
+// Package main is the entry point for the server.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/golang/glog"
+
+	"github.com/Makoto2024/project_root/golang_server/internal/server"
+
+	pb "github.com/Makoto2024/project_root/golang_server/protos/servicepb"
+)
+
+// cfg will be set via flags.
+type cfg struct {
+	IP   string
+	Port int
+}
+
+func (c *cfg) registerFlags(fs *flag.FlagSet) {
+	if fs == nil {
+		fs = flag.CommandLine
+	}
+	fs.StringVar(&c.IP, "ip", "127.0.0.1", "IP address to listen on")
+	fs.IntVar(&c.Port, "port", 8080, "Port to listen on")
+}
+
+func mainWithCfg(ctx context.Context, cfg *cfg) {
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.IP, cfg.Port))
+	if err != nil {
+		glog.ExitContextf(ctx, "Failed to listen: %v", err)
+	}
+
+	var serverOpts []grpc.ServerOption
+	grpcServer := grpc.NewServer(serverOpts...)
+	pb.RegisterHelloServiceServer(grpcServer, server.NewHelloServer())
+	reflection.Register(grpcServer)
+	grpcServer.Serve(lis)
+}
+
+func main() {
+	ctx := context.Background()
+
+	fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	var cfg cfg
+	cfg.registerFlags(fs)
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		glog.ExitContextf(ctx, "Parse command line args = %v", err)
+	}
+
+	glog.ErrorContextf(ctx, "cfg = %#v", cfg)
+	mainWithCfg(ctx, &cfg)
+}
