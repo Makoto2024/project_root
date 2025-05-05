039691d28cd2be06ef3f221e604a8f22c9b56dc8
Patrick Liao <liaopatrick@google.com>
2025-05-05 10:28:01 +0000

    Use bazel to build docker image.

--- golang_server/build/package/docker-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Assume running in project_root/
-docker build -f golang_server/build/package/Dockerfile -t "makoto2024/project_root_foo" .
--- golang_server/build/package/docker-run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -p 8080:8080 makoto2024/project_root_foo
