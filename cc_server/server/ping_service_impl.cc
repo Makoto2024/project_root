f43d05e0c89bdb6c361ecc21588b6c7515f270b9
Patrick Liao <liaopatrick@google.com>
2025-05-07 09:09:01 +0000

    Let servers to communicate with each other.
    Try bazel build //cc_server/client && bazel-bin/cc_server/client/client 1234567
    Try also bazel build //golang_server/cmd/client && bazel-bin/golang_server/cmd/client/client_/client -ip=localhost -service=pongService --param="1234567"

--- cc_server/server/ping_service_impl.cc
+++ cc_server/server/ping_service_impl.cc
@@ -1,8 +1,57 @@
 #include "cc_server/server/ping_service_impl.h"
 
+#include <grpcpp/channel.h>
+#include <grpcpp/support/status_code_enum.h>
+#include <grpcpp/support/status.h>
+#include <grpcpp/grpcpp.h>
+
+
 #include <string>
 
-#include <grpcpp/support/status.h>
+#include "absl/log/log.h"
+#include "absl/log/check.h"
+
+#include "golang_server/protos/pong_service.pb.h"
+#include "golang_server/protos/pong_service.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::Status;
+using grpc::ClientContext;
+
+
+namespace {
+
+bool call_pong(const std::string &msg, std::string &reply) {
+    constexpr char target[] = "golang-server:8080";
+    std::shared_ptr<Channel> channel =
+        grpc::CreateChannel(target, grpc::InsecureChannelCredentials());
+    if (!channel) {
+        LOG(ERROR) << "Failed to create channel to " << target << std::endl;
+        return false;
+    }
+    LOG(INFO) << "Client attempting to connect to: " << target << std::endl;
+
+
+    project_root::golang_server::PongRequest request;
+    request.set_msg(msg);
+
+    project_root::golang_server::PongResponse response;
+    grpc::ClientContext context;
+    const grpc::Status status =
+        project_root::golang_server::PongService::NewStub(channel)->Pong(&context, request, &response);
+    if (!status.ok()) {
+        LOG(ERROR)
+            << "RPC failed with error code: " << status.error_code()
+            << ", message: " << status.error_message() << std::endl;
+        return false;
+    }
+    reply = response.msg();
+    return true;
+
+}
+
+}  // namespace
+
 
 namespace project_root {
 namespace cc_server {
@@ -12,13 +61,20 @@ namespace cc_server {
     const ::project_root::cc_server::PingRequest* request,
     ::project_root::cc_server::PingResponse* response
 ) {
-
+    // If msg contains ;ping, then just return the passed in msg.
     const std::string req_msg = request->msg();
-    if (req_msg.empty()) {
-        response->set_msg("ping;@NULL@");
-    } else {
-        response->set_msg("ping;" + req_msg);
+    if (std::string::npos != req_msg.find(";ping")) {
+        response->set_msg(req_msg);
+        return ::grpc::Status::OK;
     }
+
+    // Otherwise, call PongService.Pong with {req_msg};ping
+    // and then return its responded msg.
+    std::string pong_resp_msg;
+    if (!call_pong(req_msg + ";ping", pong_resp_msg)) {
+        return ::grpc::Status::CANCELLED;
+    }
+    response->set_msg(pong_resp_msg);
     return ::grpc::Status::OK;
 }
 
--- golang_server/internal/server/pong_service.go
+++ golang_server/internal/server/pong_service.go
@@ -2,26 +2,61 @@ package server
 
 import (
 	"context"
+	"fmt"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/proto"
 
-	pb "github.com/Makoto2024/project_root/golang_server/protos/servicepb"
+	ccpb "github.com/Makoto2024/project_root/cc_server/protos/servicepb"
+	gopb "github.com/Makoto2024/project_root/golang_server/protos/servicepb"
 )
 
-var _ pb.PongServiceServer = (*PongServer)(nil) // Static check.
+var pongServiceCfg = struct {
+	Target     string
+	Credential credentials.TransportCredentials
+}{
+	Target:     "cc-server:7070",
+	Credential: insecure.NewCredentials(),
+}
+
+var _ gopb.PongServiceServer = (*PongServer)(nil) // Static check.
 
 // PongServer implements the PongService.
 type PongServer struct {
-	pb.UnimplementedPongServiceServer
+	gopb.UnimplementedPongServiceServer
 }
 
-func (s *PongServer) Pong(ctx context.Context, req *pb.PongRequest) (*pb.PongResponse, error) {
-	return pb.PongResponse_builder{
-		Msg: proto.String("pong"),
+func (s *PongServer) Pong(ctx context.Context, req *gopb.PongRequest) (*gopb.PongResponse, error) {
+	// Call PingService.Ping with `{req.Msg};pong` and return rsp.Msg.
+	conn, err := newCCServerClient()
+	if err != nil {
+		return nil, fmt.Errorf("newCCServerClient(%q): %w", pongServiceCfg.Target, err)
+	}
+	defer conn.Close()
+
+	client := ccpb.NewPingServiceClient(conn)
+	reqStr := fmt.Sprintf("%s;pong", req.GetMsg())
+	rsp, err := client.Ping(ctx, ccpb.PingRequest_builder{
+		Msg: proto.String(reqStr),
+	}.Build())
+	if err != nil {
+		return nil, fmt.Errorf("PongService.Pong(req.Msg = %q): %w", reqStr, err)
+	}
+	return gopb.PongResponse_builder{
+		Msg: proto.String(rsp.GetMsg()),
 	}.Build(), nil
 }
 
 // NewPongServer creates a new PongServer.
 func NewPongServer() *PongServer {
 	return &PongServer{}
+}
+
+func newCCServerClient() (*grpc.ClientConn, error) {
+	return grpc.NewClient(
+		pongServiceCfg.Target,
+		grpc.WithTransportCredentials(pongServiceCfg.Credential),
+	)
 }
